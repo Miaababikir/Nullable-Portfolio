058b87ddeea58e132fca627f40ecdac404f0ba9f
Miaababikir <miaababikir@gmail.com>
2019-08-27 22:29:11 +0300

    Adding nav and other sections

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'roboto': ['Roboto'],
+            },
+        }
+    },
+    variants: {},
+    plugins: []
+};
